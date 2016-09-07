5274801ba5e17dfe522a28b0ef68406142bacba5
byrdafer <crbyrd0@gmail.com>
2016-09-07 19:57:19 -0400

    add code challenge

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -6,3 +6,22 @@
 //Call function
 
 //Alert user results
+
+
+function processMessage(msg)
+{
+  if(msg === "Hello")
+  {
+    alert("Hello World");
+  };
+  else{
+    alert("You didn't say hello! :( ");
+  };
+  console.log(msg);
+};
+
+var message;
+
+message = prompt();
+
+processMessage(message);
